1d49e3b9bde856f19c9612d4cd630436ba8d522d
Idan Levian <eidan47@gmail.com>
2024-10-21 14:35:17 +0300

    Adding prettier and eslint settings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { Stack } from 'expo-router';
-import { Routes } from '@/src/types/routes';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StyleSheet } from 'react-native';
+
+import { Routes } from '@/src/types/routes';
 import { useThemeColor } from '@/src/hooks/useThemeColor';
 import BackButton from '@/src/components/BackButton/BackButton';
 
