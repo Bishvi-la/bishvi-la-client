438c9403a0cd7efa22032a3f2877211efa1c59a2
Idan Levian <41434778+eidan66@users.noreply.github.com>
2024-10-21 17:06:58 +0300

    Adding error boundry and sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,29 +5,35 @@ import { StyleSheet } from 'react-native';
 import { Routes } from '@/src/types/routes';
 import { useThemeColor } from '@/src/hooks/useThemeColor';
 import BackButton from '@/src/components/BackButton/BackButton';
+import { withSentry } from '@/src/providers/SentryProvider';
+import { ErrorBoundary } from '@/src/ErrorBoundary/ErrorBoundary';
 
-export default function RootLayout() {
+function RootLayout() {
   const backgroundColor = useThemeColor({}, 'background');
 
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor }]}>
-      <Stack
-        screenOptions={{
-          headerLeft: () => <BackButton />,
-          headerStyle: {
-            backgroundColor,
-          },
-          headerTransparent: true,
-          title: '',
-        }}
-      >
-        <Stack.Screen name={Routes.Homepage} options={{ headerShown: false }} />
-        <Stack.Screen name={Routes.Signup} />
-        <Stack.Screen name={Routes.Login} />
-      </Stack>
-    </SafeAreaView>
+    <ErrorBoundary>
+      <SafeAreaView style={[styles.container, { backgroundColor }]}>
+        <Stack
+          screenOptions={{
+            headerLeft: () => <BackButton />,
+            headerStyle: {
+              backgroundColor,
+            },
+            headerTransparent: true,
+            title: '',
+          }}
+        >
+          <Stack.Screen name={Routes.Homepage} options={{ headerShown: false }} />
+          <Stack.Screen name={Routes.Signup} />
+          <Stack.Screen name={Routes.Login} />
+        </Stack>
+      </SafeAreaView>
+    </ErrorBoundary>
   );
 }
+
+export default withSentry(RootLayout);
 
 const styles = StyleSheet.create({
   container: {
