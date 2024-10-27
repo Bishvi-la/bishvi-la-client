7cf6233af6a91b464831c8cea9c1f8e683ca771b
Idan Levian <41434778+eidan66@users.noreply.github.com>
2024-10-27 19:30:17 +0200

    Adding background image to welcome sceren

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,10 +1,10 @@
-import { useRouter } from 'expo-router';
-import React, { FunctionComponent } from 'react';
-import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
 import LOGO from '@/assets/images/bishvila-logo.png';
+import LandingPage from '@/assets/images/landingBackground.png';
 import { Routes } from '@/src/types/routes';
 import { hebrewTranslations } from '@/translation/lang-heb';
+import { useRouter } from 'expo-router';
+import React, { FunctionComponent } from 'react';
+import { Image, ImageBackground, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export const Welcome: FunctionComponent = () => {
   const router = useRouter();
@@ -15,7 +15,8 @@ export const Welcome: FunctionComponent = () => {
     <SafeAreaView style={styles.safeAreaContainer}>
       <View style={styles.container}>
         <View style={styles.logoContainer}>
-          <Image source={LOGO} width={120} height={120} />
+          <ImageBackground source={LandingPage} resizeMode={'stretch'} style={styles.imageBg} />
+          <Image source={LOGO} width={120} height={120} style={styles.logoImage} />
         </View>
         <View style={styles.buttonsContainer}>
           <TouchableOpacity style={styles.button} onPress={() => handleNavigationClick(Routes.Signup)}>
@@ -40,14 +41,28 @@ const styles = StyleSheet.create({
   safeAreaContainer: {
     flex: 1,
   },
+  imageBg: {
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    opacity: 0.8,
+  },
   container: {
     flex: 1,
   },
   logoContainer: {
     flex: 2,
     justifyContent: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.6)', // Light overlay for better contrast
   },
-
+  logoImage: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 5,
+    position: 'absolute',
+  },
   buttonsContainer: {
     marginTop: 15,
     flexDirection: 'column',
